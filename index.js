802f8d921016908aaf30d216fbea350a217239c6
Max-matix <125517747+Max-matix@users.noreply.github.com>
2023-06-16 04:16:09 -0700

    update

---
+++ index.js
@@ -0,0 +1,16 @@
+const containerElement = document.querySelector('.container');
+const btnElement = document.querySelector('.btn');
+const popupContainerElement = document.querySelector('.popup-container');
+const closeIconElement = document.querySelector('.close-icon');
+
+btnElement.addEventListener('click', () => {
+  containerElement.classList.add('active');
+  popupContainerElement.classList.remove('active');
+   
+});
+
+closeIconElement.addEventListener('click', () => {
+  containerElement.classList.remove('active');
+  popupContainerElement.classList.add('active');
+
+});
